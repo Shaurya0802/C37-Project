4015ad007664102eadf0a3be2c2034d1ff42849f
Shaurya0802 <shauryaakhileshsharma@gmail.com>
2020-05-22 15:31:03 +0530

    C36 Project

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 var dots = [];
 
+var database;
+
+var paint;
+
+var dbArray = [];
+
 function setup(){
-    canvas = createCanvas(displayWidth-70,displayHeight-150);
+    database = firebase.database();
+    canvas = createCanvas(1250,650);
+
+    paint = database.ref('drawing');
+    paint.on("value",readPosition,showError);
 }
 
 function mouseDragged(){
@@ -10,17 +20,29 @@ function mouseDragged(){
         y : mouseY
     }
     dots.push(point);
+    database.ref('drawing').set({
+        'db' : dots
+    })
+    
 }
 
 function draw(){
     background(240);
 
     beginShape();
-    strokeWeight(7);
-    stroke(0);
+    strokeWeight(10);
+    stroke(255,0,0);
     noFill();
-    for(var i = 0; i < dots.length; i++){
-        vertex(dots[i].x,dots[i].y);
+    for(var i = 0; i < dbArray.length; i++){
+        vertex(dbArray[i].x,dbArray[i].y);
     }
     endShape();
+}
+
+function readPosition(data){
+    dbArray = data.val().db;
+}
+
+function showError(){
+    console.log("Error!Error!");
 }
